55e4bceb24a9eaccb81c05e194d014907da59db6
Mahmoud-Ahmed <lya440676@gmail.com>
2022-12-16 21:22:26 +0200

    Ecommerce website

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+const index = document.getElementById('index')
+const action = document.getElementById('action')
+
+
+function isActive(){
+    index.classList.toggle('is-active')
+    action.classList.toggle('is-active')
+}
