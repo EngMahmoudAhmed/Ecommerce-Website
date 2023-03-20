8f189e70ff3639b5c4c32df6cae4bc7d034d9926
Mahmoud-Ahmed <lya440676@gmail.com>
2023-03-20 13:33:36 +0200

    adding a button to top

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,28 @@
 const index = document.getElementById('index')
 const action = document.getElementById('action')
 
+const one = document.querySelector('button')
 
 function isActive(){
     index.classList.toggle('is-active')
     action.classList.toggle('is-active')
+}
+
+
+window.onscroll = function () {
+    if(window.scrollY >= 600){
+        one.style.display = 'block'
+    }
+    else {
+        one.style.display = 'none'
+    }
+}
+
+one.onclick = function () {
+    window.scrollTo(
+        {
+            top:0,
+            behavior:"smooth"
+        }
+    )
 }
